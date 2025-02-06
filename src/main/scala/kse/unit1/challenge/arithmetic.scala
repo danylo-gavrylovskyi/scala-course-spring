70523c3ca6d7bbdcce0d1177f4e4704536a1946f
Danya <118884527+danylo-gavrylovskyi@users.noreply.github.com>
2025-02-06 18:19:56 +0200

    Implement addition, multiplication and power functions (#1)
    * feat: implement addition, multiplication and power functions
    
    * fix: signature of method of method cannot be changed
    
    * fix: add require checks

--- src/main/scala/kse/unit1/challenge/arithmetic.scala
+++ src/main/scala/kse/unit1/challenge/arithmetic.scala
@@ -23,20 +23,32 @@ object arithmetic:
       if isNonNegative(value) then value
       else -value
 
+  @tailrec
   def addition(left: Number, right: Number): Number =
     require(left >= 0, "Left must be non-negative")
     require(right >= 0, "Right must be non-negative")
 
-    ???
+    if isZero(right) then left
+    else addition(increment(left), decrement(right))
 
   def multiplication(left: Number, right: Number): Number =
     require(left >= 0, "Left must be non-negative")
     require(right >= 0, "Right must be non-negative")
 
-    ???
+    @tailrec
+    def multiplicationReq(left: Number, right: Number, accumulator: Number): Number =
+      if isZero(right) then accumulator
+      else multiplicationReq(left, decrement(right), addition(accumulator, left))
+
+    multiplicationReq(left, right, 0)
 
   def power(base: Number, p: Number): Number =
     require(p >= 0, "Power must be non-negative")
     require(base != 0 || p != 0, "0^0 is undefined")
 
-    ???
+    @tailrec
+    def powerReq(base: Number, p: Number, accumulator: Number): Number =
+      if isZero(p) then accumulator
+      else powerReq(base, decrement(p), multiplication(accumulator, base))
+
+    powerReq(base, p, 1)
