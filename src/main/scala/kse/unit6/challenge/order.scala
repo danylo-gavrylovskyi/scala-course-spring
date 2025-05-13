008f3f3e5b7bf262f4e131833f4be683c96457db
Danya <118884527+danylo-gavrylovskyi@users.noreply.github.com>
2025-05-13 13:45:06 +0300

    Implement abstract set (#7)
    * feat: implement abstract set
    
    * fix: override hashCode for proper equality checks
    
    * fix: dont use minus for comparison of numerals

--- src/main/scala/kse/unit6/challenge/order.scala
+++ src/main/scala/kse/unit6/challenge/order.scala
@@ -8,6 +8,15 @@ object order:
   trait Order[T]:
     def compare(left: T, right: T): Int
 
+  object Order:
+
+    given Order[Numeral] with
+
+      def compare(x: Numeral, y: Numeral): Int =
+        if x.toInt > y.toInt then 1
+        else if x.toInt < y.toInt then -1
+        else 0
+
   extension [V: Order as ord](elem: V)
 
     infix def >(that: V): Boolean =
--- src/main/scala/kse/unit6/challenge/set.scala
+++ src/main/scala/kse/unit6/challenge/set.scala
@@ -1,6 +1,6 @@
 package kse.unit6.challenge
 
-import kse.unit6.challenge.order.{Order, *, given}
+import kse.unit6.challenge.order.{Order, *}
 import scala.annotation.targetName
 
 object set:
@@ -39,22 +39,22 @@ object set:
   // TODO: Remind about type system
   case object Empty extends Set[Nothing]:
 
-    infix def forAll(predicate: Nothing => Boolean): Boolean = ???
+    infix def forAll(predicate: Nothing => Boolean): Boolean = true
 
-    infix def exists(predicate: Nothing => Boolean): Boolean = ???
+    infix def exists(predicate: Nothing => Boolean): Boolean = false
 
-    infix def contains[B: Order](x: B): Boolean = ???
+    infix def contains[B: Order](x: B): Boolean = false
 
-    infix def include[B: Order](x: B): Set[B] = ???
+    infix def include[B: Order](x: B): Set[B] = NonEmpty(Empty, x, Empty)
 
     // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
     //    infix def remove[B: Order](x: B): Set[B] = ???
 
     @targetName("union")
-    infix def ∪[B: Order](that: Set[B]): Set[B] = ???
+    infix def ∪[B: Order](that: Set[B]): Set[B] = that
 
     @targetName("intersection")
-    infix def ∩[B: Order](that: Set[B]): Set[B] = ???
+    infix def ∩[B: Order](that: Set[B]): Set[B] = Empty
 
     // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
     //    @targetName("difference")
@@ -62,28 +62,44 @@ object set:
 
     override def toString: String = "[*]"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean =
+      obj match
+        case _: Empty.type => true
+        case _             => false
+
+    override def hashCode: Int = 0
 
   end Empty
 
   case class NonEmpty[A](left: Set[A], element: A, right: Set[A]) extends Set[A]:
 
-    infix def forAll(predicate: A => Boolean): Boolean = ???
+    infix def forAll(predicate: A => Boolean): Boolean =
+      predicate(element) && left.forAll(predicate) && right.forAll(predicate)
 
-    infix def exists(predicate: A => Boolean): Boolean = ???
+    infix def exists(predicate: A => Boolean): Boolean =
+      predicate(element) || left.exists(predicate) || right.exists(predicate)
 
-    infix def contains[B >: A: Order](x: B): Boolean = ???
+    infix def contains[B >: A: Order](x: B): Boolean =
+      if x == element then true
+      else if x > element then right.contains(x)
+      else left.contains(x)
 
-    infix def include[B >: A: Order](x: B): Set[B] = ???
+    infix def include[B >: A: Order](x: B): Set[B] =
+      if x == element then this
+      else if x > element then NonEmpty(left, element, right.include(x))
+      else NonEmpty(left.include(x), element, right)
 
     // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
     //    infix def remove[B >: A: Order](x: B): Set[B] = ???
 
     @targetName("union")
-    infix def ∪[B >: A: Order](that: Set[B]): Set[B] = ???
+    infix def ∪[B >: A: Order](that: Set[B]): Set[B] =
+      (left ∪ (right ∪ that)).include(element)
 
     @targetName("intersection")
-    infix def ∩[B >: A: Order](that: Set[B]): Set[B] = ???
+    infix def ∩[B >: A: Order](that: Set[B]): Set[B] =
+      if that.contains(element) then ((left ∩ that) ∪ (right ∩ that)).include(element)
+      else (left ∩ that) ∪ (right ∩ that)
 
     // Optional from the Unit 5. If you haven't implement it in Unit 5 then skip it
     //    @targetName("difference")
@@ -91,6 +107,12 @@ object set:
 
     override def toString: String = s"[$left - [$element] - $right]"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean =
+      obj match
+        case s @ NonEmpty(left, v, right) => s.forAll(element => this.exists(_ == element))
+        case _                            => false
+
+    override def hashCode: Int =
+      31 * (left.hashCode + element.hashCode + right.hashCode)
 
   end NonEmpty
