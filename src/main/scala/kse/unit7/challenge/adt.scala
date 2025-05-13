cf637fe3f26ec401d82f965c2d0a3d1e3aaff6c8
Danya <118884527+danylo-gavrylovskyi@users.noreply.github.com>
2025-05-13 13:45:20 +0300

    Add implementation for PostView get methods (#8)
    * feat: add implementation for PostView get methods
    
    * fix: dont use try-catch in in map and flatMap
    
    * fix: use for-comprehension instead of flatMap and map in getPostsView method

--- src/main/scala/kse/unit7/challenge/adt.scala
+++ src/main/scala/kse/unit7/challenge/adt.scala
@@ -4,11 +4,21 @@ object adt:
 
   enum Try[+V]:
 
-    case DummyCase
+    case Success(value: V)
+    case Failure(exception: Throwable)
 
-    def flatMap[Q](f: V => Try[Q]): Try[Q] = ???
+    def flatMap[Q](f: V => Try[Q]): Try[Q] =
+      this match
+        case Success(v) => f(v)
+        case Failure(e) => Failure(e)
 
-    def map[Q](f: V => Q): Try[Q] = ???
+    def map[Q](f: V => Q): Try[Q] =
+      this match
+        case Success(v) => Success(f(v))
+        case Failure(e) => Failure(e)
 
   object Try:
-    def apply[V](v: V): Try[V] = ???
+
+    def apply[V](v: V): Try[V] =
+      try Success(v)
+      catch case e: Throwable => Failure(e)
--- src/main/scala/kse/unit7/challenge/app.scala
+++ src/main/scala/kse/unit7/challenge/app.scala
@@ -6,10 +6,39 @@ import kse.unit7.challenge.services.*
 
 object app:
 
-  def getPostsViews(apiKey: ApiKey): Try[List[PostView]] = ???
+  def getPostsViews(apiKey: ApiKey): Try[List[PostView]] =
+    for
+      profile <- getUserProfile(apiKey)
+      posts   <- getPosts(profile.userId)
+      postViews <- posts.foldLeft(Try(List.empty[PostView])) { (acc, post) =>
+        for
+          list     <- acc
+          postView <- getPostView(post)
+        yield list :+ postView
+      }
+    yield postViews
 
-  def getPostsViewDesugared(apiKey: ApiKey): Try[List[PostView]] = ???
+  def getPostsViewDesugared(apiKey: ApiKey): Try[List[PostView]] =
+    getUserProfile(apiKey).flatMap { profile =>
+      getPosts(profile.userId).flatMap { posts =>
+        posts.foldLeft(Try(List.empty[PostView])) { (acc, post) =>
+          acc.flatMap { list => getPostView(post).map(postView => list :+ postView) }
+        }
+      }
+    }
 
-  def getPostView(post: Post): Try[PostView] = ???
+  def getPostView(post: Post): Try[PostView] =
+    for
+      comments <- getComments(post.postId)
+      likes    <- getLikes(post.postId)
+      shares   <- getShares(post.postId)
+    yield PostView(post, comments, likes, shares)
 
-  def getPostViewDesugared(post: Post): Try[PostView] = ???
+  def getPostViewDesugared(post: Post): Try[PostView] =
+    getComments(post.postId).flatMap { comments =>
+      getLikes(post.postId).flatMap { likes =>
+        getShares(post.postId).map { shares =>
+          PostView(post, comments, likes, shares)
+        }
+      }
+    }
